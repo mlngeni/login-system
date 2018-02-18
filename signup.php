d972b9d7162c2ef9445d5debf60f02cdc58a6652
mlngeni <35996324+mlngeni@users.noreply.github.com>
2018-02-18 15:26:08 +0200

    Add

--- signup.php
+++ signup.php
@@ -8,15 +8,14 @@ include 'includes/signup.inc.php';
 
   
  <form method="POST">
-    <h2>Sign up</h2>
+    <h2>Sign up</h2> or <a href="login.php">Login</a>
 
 	<input type="text" name="uid" placeholder="User name...">
 	<input type="password" name="pwd" placeholder="Password....">
 
 	<button type="submit" name="signup">Sign up</button>
 	 
-	 <hr>
-	 <a href="login.php">Login</a>
+	
  </form>
 
 </div>
