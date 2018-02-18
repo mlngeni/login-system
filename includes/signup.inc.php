1b2e743960453d47ec561a1dab56720ba340741b
mlngeni <35996324+mlngeni@users.noreply.github.com>
2018-02-18 15:07:20 +0200

    Add

---
+++ includes/signup.inc.php
@@ -0,0 +1,24 @@
+<?php
+include './conf/db_conn.php';
+
+
+
+if (isset($_POST['signup'])) {
+	
+	if (!empty($_POST['uid']) || !empty($_POST['pwd'])) {
+		$qry = "INSERT INTO $table (uid, pwd) VALUES (:uid, :pwd)";
+	    $stmt = $conn->prepare($qry);
+	     $result = $stmt->execute(array(
+		   ":uid" => $_POST['uid'],
+           ":pwd" => $_POST['pwd']
+		));
+
+	   if ($result) {
+	     	echo "<h1 align='center'>sign up success</h1>";
+	     }  else{
+	     	echo "error";
+	     }
+	}else{
+          $message = "All Fields are Required";
+	}
+}
